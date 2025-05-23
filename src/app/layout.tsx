ccaf51b9fc77ec9760ed5621360b48449974a7ec
el <somto.achu5@gmail.com>
2025-05-23 23:04:21 +0100

    reverted to old image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,24 +6,24 @@ import React from 'react'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import {
-  ClerkProvider,
-  UserButton,
-  SignInButton,
-  SignUpButton,
-  SignedOut,
-  SignedIn,
-} from '@clerk/nextjs'
+// import {
+//   ClerkProvider,
+//   UserButton,
+//   SignInButton,
+//   SignUpButton,
+//   SignedOut,
+//   SignedIn,
+// } from '@clerk/nextjs'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+// const geistSans = Geist({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+// const geistMono = Geist_Mono({
+//   variable: '--font-geist-mono',
+//   subsets: ['latin'],
+// })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
