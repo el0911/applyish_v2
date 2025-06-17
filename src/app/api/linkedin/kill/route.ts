000987eae3f1ef5ee1c5266061fc041e8058f21d
el <somto.achu5@gmail.com>
2025-06-17 15:41:38 +0100

    added logs for kill

--- src/app/api/linkedin/kill/route.ts
+++ src/app/api/linkedin/kill/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as Secret) as JwtPayload;
     const { instance_name } = decoded;
 
-    consol.log('Decoded token:', decoded);
+    console.log('Decoded token:', decoded);
     console.log('Instance name:', instance_name);
 
     if (!instance_name) {
