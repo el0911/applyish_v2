dd0701c8c501b38a0eeb59b68276026e7957d22a
el <somto.achu5@gmail.com>
2025-09-25 08:24:27 +0100

    added clarity to apply page

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,6 +1,8 @@
 import FormComponent from '../components/Typeform-like/FormComponent';
+import useSprig from '@/hooks/useSprig'
 
 export default function ApplyPage() {
+  useSprig()
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 dark">
       <FormComponent />
